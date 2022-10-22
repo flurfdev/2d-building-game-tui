c77dc594fff2804ba1d9826e1fde66d04925ba89
floofywoofles <floofywoofles@floofylikesmail.xyz>
2022-10-22 15:43:00 -0400

    Added tiles class and player movement

--- src/game.ts
+++ src/game.ts
@@ -5,13 +5,64 @@ import {Position} from "./types/position.d"
 import { sleep } from "./lib/sleep"
 import readline from "readline-sync"
 
+class Tile {
+    private sprite: string;
+    private isSolid: boolean;
+    private position: Position;
+
+    constructor(sprite: string, isSolid: boolean, position: Position){
+        this.sprite = sprite;
+        this.isSolid = isSolid;
+        this.position = position;
+    }
+
+    setData(sprite: string){
+        this.sprite = sprite;
+    }
+
+    checkSolid(){
+        return this.isSolid;
+    }
+
+    getPosition(){
+        return this.position;
+    }
+}
+
+class Map {
+    private data: string[][]
+    private tiles: Array<Tile>;
+    private column: number;
+    private row: number;
+
+    constructor(data: string[][], column: number, row: number){
+        this.data = data;
+        this.tiles = [];
+        this.column = column;
+        this.row = row;
+    }
+
+    createTiles(){
+        for(let y = 0; y < this.data.length; y++){
+            for(let x = 0; x < this.data[y].length; x++){
+                const t:Tile = new Tile(this.data[y][x],(this.data[y][x] !== "-") ? true : false, {"y": y, "x": x});
+                this.tiles.push(t);
+            }
+        }
+    }
+}
+
 class Player {
     private position: Position;
     private health: number;
+    private direction: number; // 0 is left, 1 is up, 2 is right, 3 is down
+    private sprite: string;
 
-    constructor(position: Position, health: number){
+    constructor(position: Position, health: number, direction?:number, sprite?:string){
         this.position = position
         this.health = health;
+        this.direction = direction || 0;
+        this.sprite = sprite || "@";
     }
 
     setPosition(pos: Position){
@@ -21,17 +72,33 @@ class Player {
     getPosition():Position{
         return this.position
     }
+
+    getSprite():string {
+        return this.sprite;
+    }
+
+    setDirection(direction: number){
+        if(direction >= 0 && direction <= 3){
+            this.direction = direction;
+        } else {
+            console.log(`Tried to pass bad value to player direction |${direction}|. Exiting to avoid further errors`);
+            process.exit(1);
+        }
+    }
 }
 
 class Game {
     private maps: Array<MMap>;
     private currentMapPosition: RowData;
     private currMap: MMap|undefined;
+    private player: Player;
 
     constructor(){
         this.maps = [];
         this.currentMapPosition = {column: 1, row: 1}
         this.init();
+        this.findCurrentMap(this.currentMapPosition.column,this.currentMapPosition.row);
+        this.player = new Player((this.currMap?.start_position !== undefined) ? this.currMap.start_position : {"y": 1, "x": 1},10);
     }
 
     init(){
@@ -60,6 +127,8 @@ class Game {
                 return d;
             })
 
+
+
         } catch(err){
             console.log("An error occurred in Game class in the init function");
             console.error(err);
@@ -68,7 +137,7 @@ class Game {
 
     findCurrentMap(column: number, row: number){
         for(let p = 0; p < this.maps.length; p++){
-            if(this.maps[p].column == this.currentMapPosition.column && this.maps[p].row == this.currentMapPosition.row) {
+            if(this.maps[p].column===this.currentMapPosition.column && this.maps[p].row===this.currentMapPosition.row) {
                 this.currMap = this.maps[p];
             }
         }
@@ -84,33 +153,92 @@ class Game {
             console.log(map);
 
             const key: string = readline.keyIn('',{hideEchoBack: true, mask: '', limit: "wasdq "});
+            const pos: Position = this.player.getPosition();
+            let newpos:Position|undefined;
 
             switch(key){
                 case "q":
                     process.exit(0);
                     break;
+                case "w":
+                    newpos = {"y": pos.y-1, "x": pos.x};
+                    this.player.setDirection(1);
+                    break;
+                case "a":
+                    newpos = {"y": pos.y, "x": pos.x-1};
+                    this.player.setDirection(0);
+                    break;
+                case "s":
+                    newpos = {"y": pos.y+1, "x": pos.x};
+                    this.player.setDirection(3);
+                    break;
+                case "d":
+                    newpos = {"y": pos.y, "x": pos.x+1};
+                    this.player.setDirection(2);
+                    break;
+
             }
 
-            await sleep(750);
+            if(newpos){
+                if(this.isInBounds(newpos) && this.isSpotEmpty(newpos)){
+                    this.player.setPosition(newpos);
+                }
+            }
+
+            await sleep(100);
         }
     }
 
     format(map: MMap|undefined):string{
         let tmp: string = "";
 
-        if(map == undefined){
+        if(map===undefined){
             console.log("Current map is undefined for some reason");
             process.exit(1);
         }
 
         for(let y = 0; y < map.data.length; y++){
             for(let x = 0; x < map.data[y].length; x++){
+                if(y === this.player.getPosition().y && x === this.player.getPosition().x){
+                    tmp += this.player.getSprite();
+                    continue;
+                }
                 tmp += map.data[y][x];
             }
             tmp += "\n"
         }
 
         return tmp;
+    }
+
+    isSpotEmpty(pos: Position){
+        const m:MMap|undefined = this.currMap;
+
+        if(m === undefined){
+            console.log("Map variable in spot check is undefined");
+            process.exit(1);
+        }
+
+        if(m.data[pos.y][pos.x] != "#"){
+            return true;
+        }
+
+        return false;
+    }
+
+    isInBounds(pos: Position){
+        const m:MMap|undefined = this.currMap;
+
+        if(m === undefined){
+            console.log("Map variable in bounds check is undefined");
+            process.exit(1);
+        }
+
+        if(pos.y < m.data.length && pos.x < m.data[0].length){
+            return true;
+        }
+
+        return false;
     }
 }
 
